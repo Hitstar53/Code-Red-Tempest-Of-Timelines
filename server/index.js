4edb2d0edb862c84dc23b8b5e1f926e5eacbac7a
Anish <aniish9.18@gmail.com>
2024-02-16 03:01:00 +0530

    initiate

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import connectDB from './db/dbConnect.js';
 import dotenv from 'dotenv';
+import indexRouter from "./Router/index.js"
 const app = express();
 dotenv.config();
 
@@ -15,4 +16,7 @@ connectDB();
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+
+app.use("/api", indexRouter);
 
