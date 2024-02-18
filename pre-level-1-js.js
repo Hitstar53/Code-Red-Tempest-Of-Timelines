540e365b8a52e38ff29056a5c174f66fa1dc0d15
sania-rakhangi <147305583+sania-rakhangi@users.noreply.github.com>
2024-02-18 11:37:49 +0530

    Next and Replay button fixed

--- pre-level-1-js.js
+++ pre-level-1-js.js
@@ -1,6 +1,6 @@
 var text = new Array(
     "This is Agent C from the future.\nSomething has been into the systems of TPA (Temporal Protection Authority).", 
-    "We still don't know if it's a deed of a person or something else. All we have is this code: ",
+    "We still don't know if it's a deed of a person or something else. All we have is this code: \nu005400520024",
     "But this is horrifying. The thing is trying to change major historical events.",
     "And also, don't get surprised if you don't find the history as you know it, because it is not as you know it.",
     "It is not as you know it. . ."
@@ -10,36 +10,38 @@ var index = 0;
 var length = text[0].length; 
 var textPos = 0; 
 var contents = ''; 
+var txt = text[0];
 function typewriter(){
     contents =  ' ';
     var destination = document.getElementById("typedtext");
     destination.innerHTML = contents + text[index].substring(0, textPos);
     if ( textPos++ == length ) {
-        textPos = 0;
-        index++;
-        if(index==text.length){
-            setTimeout("appear()", 3000);
-            setTimeout("showButtons()", 4000);
-        }
-        if ( index != text.length ) {
-            if(index==2){
-                appear();
-                setTimeout("disappear()", 1000);
-            }
-            length = text[index].length;
-            setTimeout("typewriter()", 1000);
-        }
+        setTimeout("showButtons()", 1000);
     } else {
         setTimeout("typewriter()", speed);
     }
 }
-function appear(){
-    document.getElementById("code").style.display = "block";
-}
 function showButtons(){
     document.getElementById("buttons").style.display = "flex";
 }
-function disappear(){
-    document.getElementById("code").style.display = "none";
+function dontShowButtons(){
+    document.getElementById("buttons").style.display = "none";
+}
+function nextText(){
+    textPos = 0;
+    index++;
+    if ( index != text.length ) {
+        length = text[index].length;
+        dontShowButtons();
+        typewriter();
+    }
+    if (index==text.length) {
+        document.getElementById("replay").setAttribute("onclick", "window.location.reload()");
+        // document.getElementById("next").onclick = "";
+    }
+}
+function rePlay(){
+    textPos = 0;
+    typewriter();
 }
 typewriter();
