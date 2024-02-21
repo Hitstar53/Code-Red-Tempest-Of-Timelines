6db191d2c95c4689d96fd338c7fd82c096cca66b
Varshil Shah <varshilshah.in@gmail.com>
2024-02-21 20:12:06 +0530

    Update login UI - CLI design

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -1,27 +1,65 @@
+import { useEffect, useState } from "react";
 import styles from "./login.module.css";
 
 const LoginPage = () => {
+  const [teamCode, setTeamCode] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(function () {
+    function handleKeyPress(e) {
+      if (e.key === "Enter") {
+        handleLogin(teamCode, password);
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  });
+
+  function handleLogin(teamCode, password) {
+    if (!teamCode || !password) return;
+    console.log({ teamCode, password });
+
+    // login logic here
+
+    setTeamCode("");
+    setPassword("");
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <div className={styles.card}>
           <div className={styles.hackerImage}></div>
           <div className={styles.content}>
-            <h3 className={styles.heading}>Welcome to TPA</h3>
-            <form className={styles.formContainer}>
-              <input
-                type="text"
-                id="code"
-                placeholder="Enter team code"
-                required
-              />
-              <input
-                type="password"
-                id="password"
-                placeholder="Enter password"
-                required
-              />
-              <button type="submit">Login</button>
+            <h3 className={styles.heading}>Welcome to TPA_</h3>
+            <form className={styles.formContainer} onSubmit={handleLogin}>
+              <div className={styles.formGroup}>
+                <span>{">"}</span>
+                <input
+                  type="text"
+                  id="code"
+                  placeholder="Enter team code"
+                  value={teamCode || ""}
+                  required
+                  onChange={(e) => setTeamCode(e.target.value)}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <span>{">"}</span>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter password"
+                  value={password || ""}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <p>Press Enter to continue...</p>
             </form>
           </div>
         </div>
