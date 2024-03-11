711b971697a3da37804cb5d592cbd9f6b1e46345
Decay_Destructor <aaryanmantri29@gmail.com>
2024-03-11 22:57:21 +0530

    added 8.1

--- client/src/pages/levels/Level8_1.jsx
+++ client/src/pages/levels/Level8_1.jsx
@@ -1,138 +1,110 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Level8_1.css'
 
-const Level8_1 = () => {
-  return (
-    <div class="keypad">
-      <div class="screen">
-        <p>ENTER A VALID CODE</p>
-      </div>
-      <div class="keys"></div>
-    </div>
-  )
-}
-//Factory function to make keypad
-const keypad = (function () {
-  let inputs = [] //Stores inputs of the user for later use
+const Keypad = () => {
+  const [inputs, setInputs] = useState([])
+  const [message, setMessage] = useState('ENTER A VALID CODE')
 
-  //Initalises the keypad
-  const create = () => {
-    const keyboard = document.querySelector('.keys')
-    addNumericKeys(keyboard)
-    addLastRow(keyboard)
-  }
-
-  //Adds all numeric keys other than 0
-  const addNumericKeys = (keyboard) => {
-    for (let i = 9; i > 0; i--) {
-      const key = React.createElement('div')
-      key.classList.add('key')
-      key.setAttribute('key', i)
-      key.textContent = i
-      keyboard.appendChild(key)
+  const addInput = (key) => {
+    if (key === 'clear') {
+      setInputs([])
+    } else if (key === 'backspace') {
+      setInputs((prevInputs) => prevInputs.slice(1))
+    } else {
+      setInputs((prevInputs) => [key, ...prevInputs])
     }
   }
 
-  //Adds elements of the last row
-  const addLastRow = (keyboard) => {
-    const clearKey = document.createElement('div')
-    const zeroKey = document.createElement('div')
-    const backspaceKey = document.createElement('div')
-    clearKey.classList.add('key')
-    clearKey.classList.add('clear')
-    zeroKey.classList.add('key')
-    backspaceKey.classList.add('key')
-    backspaceKey.classList.add('backspace')
-
-    clearKey.setAttribute('key', 'clear')
-    zeroKey.setAttribute('key', '0')
-    backspaceKey.setAttribute('key', 'backspace')
-
-    clearKey.textContent = 'clear'
-    zeroKey.textContent = 0
-    backspaceKey.innerHTML = '<i class="fa-solid fa-delete-left"></i>'
-
-    keyboard.append(clearKey, zeroKey, backspaceKey)
-  }
-
-  //Listens for any clicks
-  const addListeners = () => {
-    const keys = document.querySelectorAll('.key')
-    Array.from(keys).forEach((key) => {
-      key.addEventListener('click', (e) => {
-        addInput(e.target)
-        updateScreen()
-        if (inputs.length == 20) {
-          checkCode()
-          inputs = []
-        }
-      })
-    })
-  }
-
-  //To check if entered code is valid
   const checkCode = () => {
-    if (inputs.reverse().join('') == '25501789196820303170') {
+    const reversedInputs = [...inputs].reverse().join('')
+    if (reversedInputs === '25501789196820303170') {
       displaySuccess()
     } else {
       displayIncorrect()
     }
   }
-  //Function to put keypad inputs into the inputs array
-  const addInput = (key) => {
-    if (key.getAttribute('key') == 'clear') {
-      inputs = []
-    } else if (key.getAttribute('key') == 'backspace') {
-      inputs.shift()
-    } else if (key.nodeName == 'I') {
-      inputs.shift()
-    } else {
-      inputs.unshift(key.getAttribute('key'))
-    }
-  }
 
-  //Function to display message on incorrect code
   const displayIncorrect = () => {
-    const screen = document.querySelector('.screen')
-    screen.firstChild.textContent = 'INCORRECT CODE'
+    setMessage('INCORRECT CODE')
   }
 
-  //Function to display message on correct code
   const displaySuccess = () => {
-    const screen = document.querySelector('.screen')
-    screen.firstChild.textContent = 'SUCCESS!'
+    setMessage('SUCCESS!')
   }
 
-  //Function to update screen with formated code
   const updateScreen = () => {
-    const screen = document.querySelector('.screen')
     let string = '____________________'
-    for (let i = 0; i < inputs.length; i++) {
-      let index = string.length - 1 - i
+    inputs.forEach((input, index) => {
+      const formattedIndex = string.length - 1 - index
       string =
-        string.substring(0, index) + inputs[i] + string.substring(index + 1)
-    }
-    let formatedString = ''
+        string.substring(0, formattedIndex) +
+        input +
+        string.substring(formattedIndex + 1)
+    })
+    let formattedString = ''
     for (let i = 0; i < string.length; i++) {
-      if (i == 3 || i == 7 || i == 11 || i == 15) {
-        formatedString = formatedString.concat('', `${string[i]} `)
+      if (i === 3 || i === 7 || i === 11 || i === 15) {
+        formattedString += `${string[i]} `
       } else {
-        formatedString = formatedString.concat('', string[i])
+        formattedString += string[i]
       }
     }
-    screen.firstChild.textContent = formatedString
+    setMessage(formattedString)
   }
-  return { create, addListeners }
-})()
 
-//Function calls
-keypad.create()
-keypad.addListeners()
+  const handleKeyClick = (key) => {
+    addInput(key)
+    updateScreen()
+    if (inputs.length === 20) {
+      checkCode()
+      setInputs([])
+    }
+  }
 
-//Hint as per TPA document
-window.addEventListener('load', () =>
-  console.log(
-    'Hello there! Must have been a pain getting back here. Enter all the years you have been to while chasing Andwyn in chronological order. Signs can be ignored.'
+  const addNumericKeys = () => {
+    const numericKeys = []
+    for (let i = 9; i > 0; i--) {
+      numericKeys.push(
+        <div key={i} className="key" onClick={() => handleKeyClick(String(i))}>
+          {i}
+        </div>
+      )
+    }
+    return numericKeys
+  }
+
+  const addLastRow = () => {
+    return (
+      <>
+        <div className="key clear" onClick={() => handleKeyClick('clear')}>
+          clear
+        </div>
+        <div className="key" onClick={() => handleKeyClick('0')}>
+          0
+        </div>
+        <div
+          className="key backspace"
+          onClick={() => handleKeyClick('backspace')}
+        >
+          <i className="fa-solid fa-delete-left"></i>
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <main>
+      <div className="keypad">
+        <div className="screen">
+          <p>{message}</p>
+        </div>
+        <div className="keys">
+          {addNumericKeys()}
+          {addLastRow()}
+        </div>
+      </div>
+    </main>
   )
-)
+}
 
-export default Level8_1
+export default Keypad
