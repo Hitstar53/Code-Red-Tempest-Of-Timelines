171482db2f496eb2af193e0b3701d8a443793e28
Decay_Destructor <aaryanmantri29@gmail.com>
2024-03-14 01:14:45 +0530

    a few routes

--- client/src/pages/Endings/Wrong_choice_ending.jsx
+++ client/src/pages/Endings/Wrong_choice_ending.jsx
@@ -1,24 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import './Wrong_choice_ending.css'
-
+import { useNavigate } from 'react-router-dom'
 function Wrong_choice_ending() {
-  
-  useEffect(()=>{
-    console.log("hi")
+  const navigate = useNavigate()
+  useEffect(() => {
+    console.log('hi')
     // document.addEventListener("click",()=>{
     //   document.querySelector('.img').classList.add('removing');
     // })
-    setTimeout(()=>{document.querySelector('.img').style.opacity= 0.8},3000);
-    setTimeout(()=>{document.querySelector('.img').style.opacity= 0.6},6000);
-    setTimeout(()=>{document.querySelector('.img').style.opacity= 0.4},9000);
-    setTimeout(()=>{document.querySelector('.img').style.opacity= 0.2},12000);
-    setTimeout(()=>{document.querySelector('.img').style.opacity= 0.0},15000);
-  },[]);
+    setTimeout(() => {
+      document.querySelector('.img').style.opacity = 0.8
+    }, 3000)
+    setTimeout(() => {
+      document.querySelector('.img').style.opacity = 0.6
+    }, 6000)
+    setTimeout(() => {
+      document.querySelector('.img').style.opacity = 0.4
+    }, 9000)
+    setTimeout(() => {
+      document.querySelector('.img').style.opacity = 0.2
+    }, 12000)
+    setTimeout(() => {
+      document.querySelector('.img').style.opacity = 0.0
+    }, 15000)
+    setTimeout(() => {
+      navigate('/level1')
+    }, 18000)
+  }, [navigate])
 
-  return (<>
-            <div className='img'>
-            </div>
-        </>);
+  return (
+    <>
+      <div className="img"></div>
+    </>
+  )
 }
 
 export default Wrong_choice_ending
--- client/src/pages/prelevels/Layout2.jsx
+++ client/src/pages/prelevels/Layout2.jsx
@@ -1,63 +1,64 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
-import BgAudio from "../../assets/audio/let-the-mystery-unfold.mp3";
-import styles from "./Layout2.module.css";
+import React, { useState, useEffect, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import BgAudio from '../../assets/audio/let-the-mystery-unfold.mp3'
+import styles from './Layout2.module.css'
 
 const Layout = (props) => {
-  const navigate = useNavigate();
-  const [text, setText] = useState(props.text);
-  const speed = props.speed;
-  var index = 0;
-  var textPos = 0;
-  var length = text[0].length;
-  var contents = "";
+  const navigate = useNavigate()
+  const [text, setText] = useState(props.text)
+  const speed = props.speed
+  var index = 0
+  var textPos = 0
+  var length = text[0].length
+  var contents = ''
   const rePlay = () => {
-    textPos = 0;
+    textPos = 0
     if (index >= text.length - 1) {
-      window.location.reload();
+      window.location.reload()
     }
-    typewriter();
-    dontShowButtons();
-  };
+    typewriter()
+    dontShowButtons()
+  }
   const nextText = () => {
-    textPos = 0;
-    index++;
+    textPos = 0
+    index++
     if (index !== text.length) {
-      length = text[index].length;
-      dontShowButtons();
-      typewriter();
+      length = text[index].length
+      dontShowButtons()
+      typewriter()
     }
     if (index > text.length - 1) {
-      navigate(`/levels/level${props.lvl}`);
+      console.log(props.lvl)
+      navigate(`/levels/level${props.lvl}`)
     }
-  };
+  }
   const typewriter = () => {
-    contents = " ";
-    var destination = document.querySelector("#typedtext");
-    destination.innerHTML = contents + text[index].substring(0, textPos);
+    contents = ' '
+    var destination = document.querySelector('#typedtext')
+    destination.innerHTML = contents + text[index].substring(0, textPos)
     if (textPos++ == length) {
-      setTimeout(showButtons, 1000);
+      setTimeout(showButtons, 1000)
     } else {
-      setTimeout(typewriter, speed);
+      setTimeout(typewriter, speed)
     }
-  };
+  }
   const showButtons = () => {
-    document.querySelector("#buttons").style.display = "flex";
-  };
+    document.querySelector('#buttons').style.display = 'flex'
+  }
   const dontShowButtons = () => {
-    document.querySelector("#buttons").style.display = "none";
-  };
-  const audioRef = useRef(null);
+    document.querySelector('#buttons').style.display = 'none'
+  }
+  const audioRef = useRef(null)
   useEffect(() => {
-    const audioElement = audioRef.current;
-    audioElement.volume = 0.2;
-    audioElement.play();
-    audioElement.loop = true;
-    typewriter();
+    const audioElement = audioRef.current
+    audioElement.volume = 0.2
+    audioElement.play()
+    audioElement.loop = true
+    typewriter()
     return () => {
-      audioElement.pause();
-    };
-  }, []);
+      audioElement.pause()
+    }
+  }, [])
   return (
     <div className={styles.container}>
       <audio ref={audioRef} src={BgAudio} controls={false} preload="auto">
@@ -80,7 +81,7 @@ const Layout = (props) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
