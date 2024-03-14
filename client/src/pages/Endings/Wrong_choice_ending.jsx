3217590881c72473d1832a59ddd4ebe82dfdfb2b
JashShah <78263590+TheJashShah@users.noreply.github.com>
2024-03-14 23:57:49 +0530

    Update Wrong_choice_ending.jsx

--- client/src/pages/Endings/Wrong_choice_ending.jsx
+++ client/src/pages/Endings/Wrong_choice_ending.jsx
@@ -9,19 +9,19 @@ function Wrong_choice_ending() {
     //   document.querySelector('.img').classList.add('removing');
     // })
     setTimeout(() => {
-      document.querySelector({style.img}).style.opacity = 0.8
+      document.querySelector('.img').style.opacity = 0.8
     }, 3000)
     setTimeout(() => {
-      document.querySelector({style.img}).style.opacity = 0.6
+      document.querySelector('.img').style.opacity = 0.6
     }, 6000)
     setTimeout(() => {
-      document.querySelector({style.img}).style.opacity = 0.4
+      document.querySelector('.img').style.opacity = 0.4
     }, 9000)
     setTimeout(() => {
-      document.querySelector({style.img}).style.opacity = 0.2
+      document.querySelector('.img').style.opacity = 0.2
     }, 12000)
     setTimeout(() => {
-      document.querySelector({style.img}).style.opacity = 0.0
+      document.querySelector('.img').style.opacity = 0.0
     }, 15000)
     setTimeout(() => {
       navigate('/levels/level1')
