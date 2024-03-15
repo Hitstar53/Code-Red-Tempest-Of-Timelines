6611c58ba45c90f53e75b164c71af6ae2a06b868
Decay_Destructor <aaryanmantri29@gmail.com>
2024-03-15 18:52:50 +0530

    fixed level 3.1

--- client/src/pages/levels/Level3_1.jsx
+++ client/src/pages/levels/Level3_1.jsx
@@ -3,43 +3,47 @@ import styles from './Level3_1.module.css'
 
 function Level3_1() {
   const check = () => {
-    document.querySelector('.answerTextbox').classList.remove('wrong')
-    document.querySelector('.wrongAnswer').style.display = 'none'
-    console.log(document.querySelector('.answerTextbox').value)
-    var x = document.querySelector('.answerTextbox').value
+    document
+      .querySelector(`.${styles.answerTextbox}`)
+      .classList.remove(`.${styles.wrong}`)
+    document.querySelector(`.${styles.wrongAnswer}`).style.display = 'none'
+    console.log(document.querySelector(`.${styles.answerTextbox}`).value)
+    var x = document.querySelector(`.${styles.answerTextbox}`).value
     if (x.toUpperCase() != 'HTML') {
-      document.querySelector('.answerTextbox').classList.add('wrong')
-      document.querySelector('.wrongAnswer').style.display = 'inline'
+      document
+        .querySelector(`.${styles.answerTextbox}`)
+        .classList.add(`.${styles.wrong}`)
+      document.querySelector(`.${styles.wrongAnswer}`).style.display = 'inline'
     }
   }
 
   useEffect(() => {
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     setInterval(() => {
-      document.querySelector('.lp0').innerText = document
-        .querySelector('.lp0')
+      document.querySelector(`.${styles.lp0}`).innerText = document
+        .querySelector(`.${styles.lp0}`)
         .innerText.split('')
         .map((letter) => letters[Math.floor(Math.random() * 52)])
         .join('')
 
-      document.querySelector('.lp1').innerText = document
-        .querySelector('.lp1')
+      document.querySelector(`.${styles.lp1}`).innerText = document
+        .querySelector(`.${styles.lp1}`)
         .innerText.split('')
         .map((letter) => letters[Math.floor(Math.random() * 52)])
         .join('')
 
-      document.querySelector('.lp2').innerText = document
-        .querySelector('.lp2')
+      document.querySelector(`.${styles.lp2}`).innerText = document
+        .querySelector(`.${styles.lp2}`)
         .innerText.split('')
         .map((letter) => letters[Math.floor(Math.random() * 52)])
         .join('')
 
-      document.querySelector('.lp3').innerText = document
-        .querySelector('.lp3')
+      document.querySelector(`.${styles.lp3}`).innerText = document
+        .querySelector(`.${styles.lp3}`)
         .innerText.split('')
         .map((letter) => letters[Math.floor(Math.random() * 52)])
         .join('')
-    }, 60)
+    }, 100)
   }, [])
 
   return (
@@ -52,19 +56,19 @@ function Level3_1() {
       ></link>
       <div className={styles.container}>
         <div className={styles.innerContainer}>
-          <p className={styles.q1}>
+          <p className={styles.q1 + ' ' + styles.p}>
             <span className={styles.lp0}>youtube.com/watch?v=Sag7hLly0qg</span>
           </p>
-          <p className={styles.q2}>
+          <p className={styles.q2 + ' ' + styles.p}>
             <span className={styles.lp1}>youtube.com/watch?v=A9dPtRFSfjg</span>
           </p>
-          <p className={styles.q3}>
+          <p className={styles.q3 + ' ' + styles.p}>
             <span className={styles.lp2}>https://youtu.be/68HrmbZiwaI</span>
           </p>
-          <p className={styles.q4}>
+          <p className={styles.q4 + ' ' + styles.p}>
             <span className={styles.lp3}>youtube.com/watch?v=iEmIJ3b-hvI</span>
           </p>
-          <span className={styles.entry}>
+          <span className={styles.entry + ' ' + styles.span}>
             <b>Enter your answer:</b>
           </span>
           <input
@@ -72,7 +76,9 @@ function Level3_1() {
             className={styles.answerTextbox}
             type="text"
           />
-          <span className={styles.wrongAnswer}>Wrong answer. Retry</span>
+          <span className={styles.wrongAnswer + ' ' + styles.span}>
+            Wrong answer. Retry
+          </span>
           <button className={styles.btn} type="button" onClick={check}>
             ENTER
           </button>
