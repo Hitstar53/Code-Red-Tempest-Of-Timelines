8c81b12fe8fb54fe6568a4fa1535b717ab4e3217
Decay_Destructor <aaryanmantri29@gmail.com>
2024-03-16 00:24:17 +0530

    level 8.1

--- client/src/pages/levels/Level8_1.jsx
+++ client/src/pages/levels/Level8_1.jsx
@@ -1,69 +1,69 @@
-import React, { useState } from 'react';
-import styles from './Level8_1.module.css'; // Assuming you have your CSS module file in the same directory
-import Layout from './Layout';
-import background from '../../assets/images/wepik-export-20240309064031UdkG.png';
+import React, { useState } from 'react'
+import styles from './Level8_1.module.css' // Assuming you have your CSS module file in the same directory
+import Layout from './Layout'
+import background from '../../assets/images/wepik-export-20240309064031UdkG.png'
 const Keypad = () => {
-  const [inputs, setInputs] = useState([]);
-  const [message, setMessage] = useState('ENTER A VALID CODE');
+  const [inputs, setInputs] = useState([])
+  const [message, setMessage] = useState('ENTER A VALID CODE')
 
   const addInput = (key) => {
     if (key === 'clear') {
-      setInputs([]);
+      setInputs([])
     } else if (key === 'backspace') {
-      setInputs((prevInputs) => prevInputs.slice(1));
+      setInputs((prevInputs) => prevInputs.slice(1))
     } else {
-      setInputs((prevInputs) => [key, ...prevInputs]);
+      setInputs((prevInputs) => [key, ...prevInputs])
     }
-  };
+  }
 
   const checkCode = () => {
-    const reversedInputs = [...inputs].reverse().join('');
+    const reversedInputs = [...inputs].reverse().join('')
     if (reversedInputs === '25501789196820303170') {
-      displaySuccess();
+      displaySuccess()
     } else {
-      displayIncorrect();
+      displayIncorrect()
     }
-  };
+  }
 
   const displayIncorrect = () => {
-    setMessage('INCORRECT CODE');
-  };
+    setMessage('INCORRECT CODE')
+  }
 
   const displaySuccess = () => {
-    setMessage('SUCCESS!');
-  };
+    setMessage('SUCCESS!')
+  }
 
   const updateScreen = () => {
-    let string = '____________________';
+    let string = '____________________'
     inputs.forEach((input, index) => {
-      const formattedIndex = string.length - 1 - index;
+      const formattedIndex = string.length - 1 - index
       string =
         string.substring(0, formattedIndex) +
         input +
-        string.substring(formattedIndex + 1);
-    });
-    let formattedString = '';
+        string.substring(formattedIndex + 1)
+    })
+    let formattedString = ''
     for (let i = 0; i < string.length; i++) {
       if (i === 3 || i === 7 || i === 11 || i === 15) {
-        formattedString += `${string[i]} `;
+        formattedString += `${string[i]} `
       } else {
-        formattedString += string[i];
+        formattedString += string[i]
       }
     }
-    setMessage(formattedString);
-  };
+    setMessage(formattedString)
+  }
 
   const handleKeyClick = (key) => {
-    addInput(key);
-    updateScreen();
+    addInput(key)
+    updateScreen()
     if (inputs.length === 20) {
-      checkCode();
-      setInputs([]);
+      checkCode()
+      setInputs([])
     }
-  };
+  }
 
   const addNumericKeys = () => {
-    const numericKeys = [];
+    const numericKeys = []
     for (let i = 9; i > 0; i--) {
       numericKeys.push(
         <div
@@ -73,10 +73,10 @@ const Keypad = () => {
         >
           {i}
         </div>
-      );
+      )
     }
-    return numericKeys;
-  };
+    return numericKeys
+  }
 
   const addLastRow = () => {
     return (
@@ -100,8 +100,8 @@ const Keypad = () => {
           Del
         </div>
       </>
-    );
-  };
+    )
+  }
 
   return (
     <Layout
@@ -111,19 +111,16 @@ const Keypad = () => {
       score="69420"
       backgroundPicURL={background}
       colors={{
-        textColor: '#e699ba',
+        textColor: 'white',
         boxBackgroundColor: '#642d2c',
         leaderboardHeaderColor: 'white',
-        iconColor: '#e699ba',
+        iconColor: 'white',
         leaderboardPositionColor: '#CCD3CA',
         hintTextColor: 'white',
         leaderboardColor: '#76453B',
         leaderboardTextColor: 'black',
       }}
-      hintText="There is one file, there are two files, or however many there are… just observe the files and escape patterns carefully. Kim Jong Un’s final destination lies in plain sight.
-
-Also, before submitting just remember: “Are we in North Korea or are we in New Delhi?”
-"
+      hintText="We know we started the chase to Andwyn from 2550 BCE in Egypt, and last we knew, we were in 21st century North Korea. Just buckle up and enter the calendar years of our journey in travel order."
     >
       <div className={styles.keypad}>
         <div className={styles.screen}>
@@ -135,7 +132,7 @@ Also, before submitting just remember: “Are we in North Korea or are we in New
         </div>
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default Keypad;
+export default Keypad
