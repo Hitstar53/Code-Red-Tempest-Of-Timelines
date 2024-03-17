9c974ad9999c44b6c2450d18c693cc7847289659
Aaryan Urunkar <aaryanman05@gmail.com>
2024-03-17 18:13:54 +0530

    Pre level commit

--- client/src/pages/prelevels/Prelevel5.jsx
+++ client/src/pages/prelevels/Prelevel5.jsx
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 
 const Prelevel5 = () => {
   const text = [
-    "Agent C: Congratulations on completing yet another challenge! You may have saved Paris, but the hunt is far from over. Now that you have defused the explosive, please let me examine it more closely to find any clues.",
+    "Congratulations on completing yet another challenge! You may have saved Paris, but the hunt is far from over. Now that you have defused the explosive, please let me examine it more closely to find any clues.",
     "Hmm. I found this number written on the backside of the explosive, 64638336 74989 34448, and a message: 'Zivz 51, Mvezwz' .I cannot seem to make anything out of it. Are you able to? Reminds me of some random keys pressed on a 20th century device called a phone...",
   ];
   return (
