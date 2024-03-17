eeec34eaa5afb169e5680670dccb542c483b5178
Anish <aniish9.18@gmail.com>
2024-03-17 19:58:39 +0530

    done 3-2

--- client/src/pages/levels/Level3_2.jsx
+++ client/src/pages/levels/Level3_2.jsx
@@ -11,34 +11,52 @@ import { set } from 'lodash'
 const Level3_2 = () => {
   const navigate = useNavigate()
   const [text, setText] = useState('');
+  const [typing, setTyping] = useState(false);
+  const [displayedText, setDisplayedText] = useState('');
   const [index, setIndex] = useState(0);
   const [charIndex, setCharIndex] = useState(0);
   const [isTyping, setIsTyping] = useState(true);
   const [search, setSearch] = useState('')
   const [score,setScore] = useState(0)
 
+  const textArray = [
+    '-36.889889 145.184472',
+    '-37.993139 141.721556',
+    '-38.313389 145.140417',
+    '-37.897028 144.746028',
+    '-37.845139 144.995972',
+    '-37.993139 141.721556'
+  ];
+
+  const startTyping = () => {
+    let arrayIndex = 0;
+    let textIndex = 0;
+    setTyping(true);
+
+    const intervalId = setInterval(() => {
+      if (arrayIndex < textArray.length) {
+        const text = textArray[arrayIndex];
+        if (textIndex < text.length) {
+          setDisplayedText((prev) => prev + text.charAt(textIndex));
+          textIndex++;
+        } else {
+          arrayIndex++;
+          textIndex = 0;
+          setDisplayedText('');
+        }
+      } else {
+        clearInterval(intervalId);
+        setTyping(false);
+      }
+    }, 300);
+
+    return () => clearInterval(intervalId); // Cleanup function if you were to use this pattern in useEffect
+  };
+
   useEffect(() => {
     getCurrentLevel()
     getScore().then((res) => {setScore(res)})
-    const handleKeyPress = async (e) => {
-      if (e.key === 'Enter') {
-        if (await Level3_2Sol(search)){
-          if (await checkisLooped()){
-            await incrementLevel()
-            navigate('/levels/prelevel4')
-          }
-          else{
-            await updateLevel()
-            navigate('/levels/prelevel4')
-          }
-        }
-      }
-    }
-    window.addEventListener('keydown', handleKeyPress)
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress)
-    }
-  }, [search, navigate])
+  }, [])
 
   const handleCheck = async () => {
     console.log('here')
@@ -94,12 +112,14 @@ const Level3_2 = () => {
   //   return () => clearInterval(interval);
   // }, [index, charIndex, isTyping]);
 
+
+ 
   return (
     <Layout
       level={3}
-      name="Team Gods"
+      name= {localStorage.getItem("team")? JSON.parse(localStorage.getItem("team")).name : "Team Name"}
         time="00:00:00"
-        score="69420"
+        score={parseInt(score)}
         colors={{
           textColor: '#3b2a1a',
           boxBackgroundColor: '#c3a077',
@@ -113,13 +133,18 @@ const Level3_2 = () => {
       backgroundPicURL={backgroundPicURL}
       hintText={hintText}
     >
-      <div className={styles.text}>
-      {text}
-      </div>
+     
+        <div
+         className={styles.text}
+        >{displayedText}</div>
       <div className={styles.inputContainer}>
         <input className={styles.level2input} value={search} type="text" placeholder="Enter Name" onChange={(e) => setSearch(e.target.value)}/>
         <button className={styles.level2btn} type="submit" onClick={handleCheck}>
+
           Login
+        </button>
+        <button className={styles.level2btn} onClick={() => !typing && startTyping()}>
+          Flash Info
         </button>
       </div>
     </Layout>
