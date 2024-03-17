2a2978a733688d4893498410a9fdca87f9530e30
vinzi0812 <vineetjparmar@gmail.com>
2024-03-17 11:34:48 +0530

    changes

---
+++ client/src/pages/prelevels/Prebackstory.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "./Layout";
+
+const Prelevel1 = () => {
+  const text = [
+    "Phew! That was a close call. Let me search more about this Andwyn guy. There must be something in the archives.",
+    "I've found something. It's a video of Andwyn. Let me play it for you.",
+  ];
+  return (
+    <div>
+      <Layout text={text} speed={90} lvl={1} />
+    </div>
+  );
+};
+
+export default Prelevel1;
