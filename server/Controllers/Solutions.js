b86e119edbd1985afa8c396996bcca0e4eb2c7bb
Anish <aniish9.18@gmail.com>
2024-03-18 12:28:29 +0530

    3170

--- server/Controllers/Solutions.js
+++ server/Controllers/Solutions.js
@@ -72,7 +72,7 @@ const Level7 = async (req, res) => {
 
 const Level8_1 = async (req, res) => {
     const {answer} = req.body;
-    if (answer === '26001789196820303010'){
+    if (answer === '26001789196820303170'){
         return res.status(200).json({data:true});
     }
     return res.status(400).json({data:false});
